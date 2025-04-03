d657bd7488a7501cafec9fba6d7f2cea079b9ce7
Nrup <nruppatel2004@gmail.com>
2025-04-03 19:56:09 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+
+ export default {
+    content: ["./src/**/*.{html,js}", "./index.html"],
+    theme: {
+        screens: {
+            sm: '480px',
+            md: '720px',
+            lg: '976px',
+            xl: '1440px'
+        },
+        extend: {
+            colors: {
+                brightred: 'hsl(12, 88%, 59%)',
+                brightRedLight: 'hsl(12, 88%, 69%)',
+                brightRedSupLight: 'hsl(12, 88%, 95%)',
+                darkBlue: 'hsl(228, 38%, 23%)',
+                darkGrayishBlue: 'hsl(227, 12%, 61%)',
+                veryDarkBlue: 'hsl(233, 12%, 13%)',
+                veryPaleRed: 'hsl(13, 100%, 96%)',
+                veryLightGray: 'hsl(0, 0%, 98%)'
+            }
+        },
+    },
+    plugins: [],
+  }
